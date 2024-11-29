975dae54e116acd5aa2689bddc6284d6e2940997
mickaelgall <mickaelgall6@gmail.com>
2024-11-29 16:22:11 +0100

    projetPHP

--- connexion.php
+++ connexion.php
@@ -1,9 +1,20 @@
-<!DOCTYPE html>
-<html>
-<body>
 <?php
 
+// Connexion au serveur
+
+try {
+
+$dns = 'mysql:host=localhost;dbname=phpmicka'; // dbname : nom de la base
+$utilisateur = 'root'; // root sur vos postes
+$motDePasse = ''; // pas de mot de passe sur vos postes
+$connexion = new PDO( $dns, $utilisateur, $motDePasse );
+
+} catch (Exception $e) {
+
+  echo "Connexion à MySQL impossible : ", $e->getMessage();
+
+  die();
+
+}
 
 ?>
-</body>
-</html>
